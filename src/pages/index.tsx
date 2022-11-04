226af062a74ab631eecba1add16cc6868f6d2131
Joao Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-11-04 13:39:52 -0300

    updates web

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
 export default function Home() {
+  
+  fetch('http://localhost:3333/pools/count')
+  .then(response => response.json)
+  .then(data => {
+    console.log(data);
+  })
+
   return (
     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, tempora deserunt. Molestias, unde? Repellendus iusto ducimus dignissimos quos repellat? Unde suscipit officia, saepe repellendus placeat harum! Amet ipsum voluptatibus quaerat. </h1>
   )
